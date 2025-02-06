f3bd534df6149a0a7593fcccf024f77a3a43c072
omarmechergui <oo2377107@gmail.com>
2025-02-06 19:15:48 +0100

    Update config.js

--- fonfuguration/config.js
+++ fonfuguration/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = async()=>{
     try{
-        await mongoose.connect('mongodb+srv://omaar:TmYTxhxY3qlPbUgZ@cluster0.rdjpu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('..............');
         console.log('Connected to MongoDB');
     }catch(error){
         console.error('Error connecting to MongoDB:', error);
